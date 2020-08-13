722c7f4cd71a428840b61969d67d474db4e744cf
Arotăriței Robert, R <r.arotaritei@student.fontys.nl>
2020-08-13 15:25:38 +0200

    Add Player Movement and Boundaries

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+
+    private void Awake()
+    {
+        rb=GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0));
+        rb.AddForce(new Vector2(0, Input.GetAxis("Vertical") * speed));
+    }
+}
