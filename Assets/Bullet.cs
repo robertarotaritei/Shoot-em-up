b4e32f5241f3d9ae56b33390f8cdfc7d8fe65d16
Arotăriței Robert, R <r.arotaritei@student.fontys.nl>
2020-08-17 21:07:04 +0200

    Add Enemy Spawner

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public int bulletDamage;
+    private void Awake()
+    {
+        Destroy(gameObject, 2.5f);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
---
+++ Assets/Spawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public float spawnRate;
+    public GameObject[] enemies;
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", 2, spawnRate);
+        spawnRate -= 0.2f;
+    }
+    void SpawnEnemy()
+    {
+
+        int sideOfSpawning = Random.Range(-1, 2);
+        if(sideOfSpawning == 0)
+        {
+            Instantiate(enemies[Random.Range(0, enemies.Length)], new Vector3(Random.Range(-8.5f, 8.5f), 7.5f, 0), Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(enemies[Random.Range(0, enemies.Length)], new Vector3(sideOfSpawning * 10, Random.Range(-4.2f, 4.2f), 0), Quaternion.identity);
+        }
+    }
+}
