38f6ea5ec6cf99bdf04c49d2cd40b5b31225ff0f
Arotăriței Robert, R <r.arotaritei@student.fontys.nl>
2020-08-19 11:56:26 +0200

    Update Enemy Spawner

--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -6,9 +6,11 @@ public class Spawn : MonoBehaviour
 {
     public float spawnRate;
     public GameObject[] enemies;
+    int differentEnemies = 0;
     void Start()
     {
         InvokeRepeating("SpawnEnemy", 2, spawnRate);
+        StartCoroutine(MyCounter(enemies.Length));
         spawnRate -= 0.2f;
     }
     void SpawnEnemy()
@@ -17,11 +19,21 @@ public class Spawn : MonoBehaviour
         int sideOfSpawning = Random.Range(-1, 2);
         if(sideOfSpawning == 0)
         {
-            Instantiate(enemies[Random.Range(0, enemies.Length)], new Vector3(Random.Range(-8.5f, 8.5f), 7.5f, 0), Quaternion.identity);
+            Instantiate(enemies[Random.Range(0, differentEnemies)], new Vector3(Random.Range(-8.5f, 8.5f), 7.5f, 0), Quaternion.identity);
         }
         else
         {
-            Instantiate(enemies[Random.Range(0, enemies.Length)], new Vector3(sideOfSpawning * 10, Random.Range(-4.2f, 4.2f), 0), Quaternion.identity);
+            Instantiate(enemies[Random.Range(0, differentEnemies)], new Vector3(sideOfSpawning * 10, Random.Range(-4.2f, 4.2f), 0), Quaternion.identity);
+        }
+    }
+
+    IEnumerator MyCounter(int number)
+    {
+        int i = 0;
+        while (i < number)
+        {
+            differentEnemies++;
+            yield return new WaitForSeconds(15);
         }
     }
 }
