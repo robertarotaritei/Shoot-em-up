358623c5e40c3c390a12c0fd4634661919bcf21e
Arotăriței Robert, R <r.arotaritei@student.fontys.nl>
2020-09-01 23:41:21 +0200

    Update Death Explosions

---
+++ Assets/Scripts/DestroySelf.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour
+{
+    public float time;
+    void Start()
+    {
+        Destroy(gameObject, time);
+    }
+}
